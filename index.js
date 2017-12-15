af8f8de4135257400c2f3d4362da7f698b4d1890
Caleb Duckwall <caleb.duckwall@liveoakbank.com>
2017-12-14 19:14:04 -0500

    Updating package

--- index.js
+++ index.js
@@ -1,89 +1,85 @@
-let cheerio = require('cheerio'),
-    https = require('./request'),
+var cheerio = require('cheerio'),
+    request = require('request'),
     url = 'http://www.wblivesurf.com/',
-    Request = new https.request({ url: url, contentType: 'text/html' }),
     logoCount = 0,
-    body = {};
+    data = {};
 
 exports.handler = (event, context, callback) => {
 
-    Request.promise()
-        .then(response => {
-            let $ = cheerio.load(response);
+    request(url, function (error, response, body) {
 
-            //Get the date of the report
-            $('.date').filter(function () {
-                let date = $(this).text().split('/');
-                body.date = date[1].trim();
-            });
+        var $ = cheerio.load(body);
 
-            //Get the time of the report
-            $('.time').filter(function () {
-                let time = $(this).text().split('/');
-                body.time = time[0].trim();
-            });
-
-            //Get the report description
-            $('#wbMainReport .item').each(function (i, elem) {
-                $('.date', $(this)).remove();
-                $('h3', $(this)).remove();
-                $('.time', $(this)).remove();
-                body.desc = $(this).text().split(":").pop().trim();
-            });
-
-            //Get the logos
-            $('.current div').each(function (i, elem) {
-
-                if (this.attribs.class == 'ratingCell half') {
-                    logoCount += 0.5;
-                }
-                else if (this.attribs.class == 'ratingCell') {
-                    logoCount += 1;
-                }
-
-                body.logoCount = logoCount;
-
-            }).attr('class');
-
-            //Get other information
-            $('p', '#wbMainRating').each(function (i, elem) {
-
-                let info = $(this).text().split(':');
-
-                switch (info[0]) {
-                    case 'Swell Size':
-                        body.size = info[1].trim();
-                        break;
-                    case 'Water Surface':
-                        body.surface = info[1].trim();
-                        break;
-                    case 'Water Temp':
-                        body.waterTemp = info[1].trim();
-                        break;
-                    case 'Wind':
-                        body.wind = info[1].trim();
-                        break;
-                    case 'High Tide':
-                        body.highTide = `${info[1]}:${info[2]}:${info[3]}`.trim();
-                        break;
-                    case 'Low Tide':
-                        body.lowTide = `${info[1]}:${info[2]}:${info[3]}`.trim();
-                        break;
-                    case 'Sunrise':
-                        body.sunrise = `${info[1]}:${info[2]}:${info[3]}`.trim();
-                        break;
-                    case 'Sunset':
-                        body.sunset = `${info[1]}:${info[2]}:${info[3]}`.trim();
-                        break;
-                }
-
-            });
-
-            callback(null, body);
-
-        })
-        .catch(err => {
-            callback(null, err);
+        //Get the date of the report
+        $('.date').filter(function () {
+            var date = $(this).text().split('/');
+            data.date = date[1].trim();
         });
+
+        //Get the time of the report
+        $('.time').filter(function () {
+            var time = $(this).text().split('/');
+            data.time = time[0].trim();
+        });
+
+        //Get the report description
+        $('#wbMainReport .item').each(function (i, elem) {
+            $('.date', $(this)).remove();
+            $('h3', $(this)).remove();
+            $('.time', $(this)).remove();
+            data.desc = $(this).text().split(":").pop().trim();
+        });
+
+        //Get the logos
+        $('.current div').each(function (i, elem) {
+
+            if (this.attribs.class == 'ratingCell half') {
+                logoCount += 0.5;
+            }
+            else if (this.attribs.class == 'ratingCell') {
+                logoCount += 1;
+            }
+
+            data.logoCount = logoCount;
+
+        }).attr('class');
+
+        //Get other information
+        $('p', '#wbMainRating').each(function (i, elem) {
+
+            var info = $(this).text().split(':');
+
+            switch (info[0]) {
+                case 'Swell Size':
+                    data.size = info[1].trim();
+                    break;
+                case 'Water Surface':
+                    data.surface = info[1].trim();
+                    break;
+                case 'Water Temp':
+                    data.waterTemp = info[1].trim();
+                    break;
+                case 'Wind':
+                    data.wind = info[1].trim();
+                    break;
+                case 'High Tide':
+                    data.highTide = `${info[1]}:${info[2]}:${info[3]}`.trim();
+                    break;
+                case 'Low Tide':
+                    data.lowTide = `${info[1]}:${info[2]}:${info[3]}`.trim();
+                    break;
+                case 'Sunrise':
+                    data.sunrise = `${info[1]}:${info[2]}:${info[3]}`.trim();
+                    break;
+                case 'Sunset':
+                    data.sunset = `${info[1]}:${info[2]}:${info[3]}`.trim();
+                    break;
+            }
+
+        });
+
+        callback(data);
+
+    });
 
 }
